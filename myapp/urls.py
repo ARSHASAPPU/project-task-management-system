d67a3c12ed674658eca11b0bf289e5fda8afb823
ARSHASAPPU <arshaasappu@gmail.com>
2025-06-21 21:34:57 +0530

    linked admin access page and register page

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('role/',views.role,name='role'),
     path('login/',views.login_page,name='user_login'),
     path('admin_access/',views.admin_access,name='admin_access_page'),
-    path('register/',views.register_page),
+    path('register/',views.register_page,name='register_page'),
     
     path('admin_dash/',views.admin_dash),
     path('admin_project/',views.admin_project),
